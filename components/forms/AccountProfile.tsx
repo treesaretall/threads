76c1a50a39721846a840e4c522b6e638fae58a05
treesaretall <himatthewhurst@hotmail.com>
2023-09-21 07:57:35 -0400

    started on onboarding page and AccountProfile

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Profiler } from "react";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  return <div>Account Profile</div>;
+};
+
+export default AccountProfile;
